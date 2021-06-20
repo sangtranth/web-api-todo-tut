a390fb869f635c3102f23fefdb34413a5c7c3670
Sang Tran Thanh <sangtranthanh@users-MacBook-Pro.local>
2021-06-20 22:47:54 +0700

    commit code

---
+++ TodoApi/Models/TodoItemDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TodoApi.Models
+{
+    public class TodoItemDTO
+    {
+        public TodoItemDTO()
+        {
+
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
